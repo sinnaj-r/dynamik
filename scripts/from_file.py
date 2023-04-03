81e4571190746e3696f45dec593eee0baf1b529f
Victor Jose Gallego Fontenla <victorjose.gallego@usc.es>
2023-04-03 18:33:09 +0300

    Update experiments runner

--- scripts/from_file.py
+++ scripts/from_file.py
@@ -1,21 +1,18 @@
 from __future__ import annotations
 
 import time
+import typing
 from datetime import timedelta
 
 from expert.drift import detect_drift
 from expert.drift.causes import explain_drift
 from expert.drift.model import DriftCauses
 from expert.drift.plot import plot_causes
-from expert.input.csv import DEFAULT_APROMORE_CSV_MAPPING, read_and_merge_csv_logs
+from expert.input.csv import DEFAULT_CSV_MAPPING, read_and_merge_csv_logs
 from expert.logger import LOGGER, Level, setup_logger
-from expert.utils import (
-    compute_average_case_duration,
-    compute_average_inter_case_time,
-    infer_final_activities,
-    infer_initial_activities,
-)
+from expert.model import Event
 from expert.utils.concurrency import HeuristicsConcurrencyOracle
+from expert.utils.log import infer_final_activities, infer_initial_activities
 
 
 def __print_causes(_causes: DriftCauses) -> None:
@@ -25,37 +22,40 @@ def __print_causes(_causes: DriftCauses) -> None:
     LOGGER.notice("    arrival rate changed: %s", _causes.arrival_rate_changed)
     LOGGER.notice("    resource utilization rate changed: %s", _causes.resource_utilization_rate_changed)
 
-if __name__ == '__main__':
+def __preprocess(event_log: typing.Iterable[Event]) -> typing.Iterable[Event]:
+    event_log = list(event_log)
+
+    for event in event_log:
+        if event.resource == "Fake_Resource":
+            event.resource = None
+
+    return HeuristicsConcurrencyOracle(event_log).compute_enablement_timestamps()
+
+if __name__ == "__main__":
     start = time.perf_counter_ns()
 
-    setup_logger(Level.INFO)
+    setup_logger(Level.VERBOSE)
 
     files = (
-        ("log1", "../data/simple/base.csv"),
+        ("batch", "../data/simple/batches.csv"),
     )
 
-    log = read_and_merge_csv_logs(
-        files,
-        attribute_mapping=DEFAULT_APROMORE_CSV_MAPPING,
-        preprocessor=lambda evt_log: HeuristicsConcurrencyOracle(evt_log).compute_enablement_timestamps(),
+    log = list(
+        read_and_merge_csv_logs(
+            files,
+            attribute_mapping=DEFAULT_CSV_MAPPING,
+            preprocessor=__preprocess,
+        ),
     )
-
-    num_cases = 200
-
-    log = list(log)
-    initial_activities = infer_initial_activities(log)
-    final_activities = infer_final_activities(log)
-    avg_case_duration = compute_average_case_duration(log)
-    time_between_cases = compute_average_inter_case_time(log, initial_activities)
 
     detector = detect_drift(
         log=(event for event in log),
-        timeframe_size=num_cases * time_between_cases + avg_case_duration,
-        overlap_between_models=(num_cases * time_between_cases + avg_case_duration) / 2,
-        warm_up=num_cases * time_between_cases,
-        warnings_to_confirm=3,
-        initial_activities=initial_activities,
-        final_activities=final_activities,
+        timeframe_size=timedelta(days=7),
+        overlap_between_models=timedelta(days=7) / 2,
+        warm_up=timedelta(days=7),
+        warnings_to_confirm=5,
+        initial_activities=infer_initial_activities(log),
+        final_activities=infer_final_activities(log),
     )
 
     for index, drift in enumerate(detector):
