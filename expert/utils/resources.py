4fdad5a0abe9c94057b2af42c1238ba42c751a6b
Victor Jose Gallego Fontenla <victorjose.gallego@usc.es>
2023-04-17 19:00:08 +0300

    Add code to compute resources calendars

--- expert/utils/resources.py
+++ expert/utils/resources.py
@@ -1,7 +1,7 @@
 """This module provides some resource-related utilities"""
 import typing
 from collections import defaultdict
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
 from math import ceil
 
 import pandas as pd
@@ -15,7 +15,8 @@ def compute_resources_utilization_rate(
         log: typing.Iterable[Event],
         timeunit: timedelta = timedelta(minutes=1),
 ) -> typing.Mapping[str, typing.Iterable[float]]:
-    """Compute the mean resource utilization for each resource in the event log.
+    """
+    Compute the mean resource utilization for each resource in the event log.
 
     To perform this computation, the log timeframe is split in slots of size `timeunit` and, for each of these slots,
     the rate of occupation is computed by intersecting it with the events executed by each resource. Finally, the mean
@@ -60,3 +61,81 @@ def compute_resources_utilization_rate(
     return resources_occupancy
 
 
+def compute_resources_availability_calendars(
+        log: typing.Iterable[Event],
+        granularity: timedelta=timedelta(minutes=1),
+) -> typing.Mapping[str, typing.Mapping[int, typing.Mapping[tuple[time, time], bool]]]:
+    """
+    Compute the availability calendar for each resource in the log.
+
+    Parameters
+    ----------
+    * `log`:            *an event log*
+    * `granularity`:    *the granularity used to compute the resources utilization rate
+
+    Returns
+    -------
+    * a mapping with pairs (`resource`, `calendar`) where calendar is an iterable of booleans indicating if the resource
+    was available at the interval i for intervals of size granularity
+    """
+    # build a map with the events executed by each resource
+    events_per_resource = defaultdict(list)
+    for event in log:
+        events_per_resource[event.resource].append(event)
+
+    # compute the calendar for each resource
+    calendars = {}
+    for (resource, events) in events_per_resource.items():
+        calendars[resource] = __compute_calendar(events, granularity)
+
+    return calendars
+
+
+def __compute_calendar(
+        log: typing.Iterable[Event],
+        granularity: timedelta=timedelta(minutes=1),
+) -> typing.Mapping[int, typing.Mapping[tuple[time, time], bool]]:
+    calendar: dict[int, dict[tuple[time, time], bool]] = defaultdict(dict)
+
+    for event in log:
+        # store the event start and end timestamps
+        start = event.start.time()
+        end = event.end.time()
+        # store the start and end weekdays
+        start_weekday = event.start.weekday()
+        end_weekday = event.end.weekday()
+        # compute the start and end slots for given granularity
+        start_slot_idx = int((start.second + start.minute * 60 + start.hour * 60 * 60) // granularity.total_seconds())
+        end_slot_idx = int((end.second + end.minute * 60 + end.hour * 60 * 60) // granularity.total_seconds())
+
+        start_slot: tuple[time, time] = (
+            time(
+                hour=int(((start_slot_idx * granularity.total_seconds()) // 3600) % 24),
+                minute=int(((start_slot_idx * granularity.total_seconds()) // 60) % 60),
+                second=int((start_slot_idx * granularity.total_seconds()) % 60),
+            ),
+            time(
+                hour=int(((start_slot_idx * granularity.total_seconds() + granularity.total_seconds()) // 3600) % 24),
+                minute=int(((start_slot_idx * granularity.total_seconds() + granularity.total_seconds()) // 60) % 60),
+                second=int((start_slot_idx * granularity.total_seconds() + granularity.total_seconds()) % 60),
+            ),
+        )
+
+        end_slot: tuple[time, time] = (
+            time(
+                hour=int(((end_slot_idx * granularity.total_seconds()) // 3600) % 24),
+                minute=int(((end_slot_idx * granularity.total_seconds()) // 60) % 60),
+                second=int((end_slot_idx * granularity.total_seconds()) % 60),
+            ),
+            time(
+                hour=int(((end_slot_idx * granularity.total_seconds() + granularity.total_seconds()) // 3600) % 24),
+                minute=int(((end_slot_idx * granularity.total_seconds() + granularity.total_seconds()) // 60) % 60),
+                second=int((end_slot_idx * granularity.total_seconds() + granularity.total_seconds()) % 60),
+            ),
+        )
+
+        # set the slots for the event start and end as active
+        calendar[start_weekday][start_slot] = True
+        calendar[end_weekday][end_slot] = True
+
+    return calendar
